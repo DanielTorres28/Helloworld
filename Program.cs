93ec7b9d246a4c6b943ca7d59db4aa57316cbbe7
DanielTorres28 <daniel.vic1@hotmail.com>
2022-03-08 23:00:41 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog01_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Boa Noite!!!"); // Barra > Arrab
+            Console.WriteLine();
+            Console.Write("\n\nPressione qualquer tecla para encerrar este programa...");
+            Console.ReadKey(); // esta leitura de tecla serve para dar uma paradinha, para que o usário veja a execução
+        } 
+    }
+}
